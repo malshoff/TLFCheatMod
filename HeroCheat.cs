e1179a36e9542f4768fe6a68e06cf3a392bfcbc5
malshoff <malshoff5@gmail.com>
2025-01-12 18:57:55 -0800

    restore original stats when unchecking enable

--- HeroCheat.cs
+++ HeroCheat.cs
@@ -2,9 +2,8 @@
 using HarmonyLib;
 using UnityEngine;
 using System.Collections.Generic;
-using BepInEx.Unity.Mono;
 using BepInEx.Logging;
-
+using BepInEx.Unity.Mono;
 
 namespace MyFirstPlugin {
     [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
@@ -15,26 +14,24 @@ namespace MyFirstPlugin {
         private Harmony harmony;
         public bool enablePatch = false; // Global toggle for the patch
         public Dictionary<Players_FindNearestAndAttack, bool> instanceStates = new Dictionary<Players_FindNearestAndAttack, bool>();
+        public Dictionary<Players_FindNearestAndAttack, (int Hp, float critChance, float lifeSteal)> originalStats =
+            new Dictionary<Players_FindNearestAndAttack, (int, float, float)>();
 
         private void Awake() {
- 
             Instance = this;
             Logger = base.Logger;
 
             // Initialize Harmony
             harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
-            harmony.PatchAll(); 
-
+            harmony.PatchAll();
 
             Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
         }
 
         private void OnGUI() {
-
             GUILayout.BeginArea(new Rect(10, 10, 300, 400), GUI.skin.box);
 
             GUILayout.Label("Hero Patch Controls");
-
 
             enablePatch = GUILayout.Toggle(enablePatch, "Enable Patch");
 
@@ -50,10 +47,13 @@ namespace MyFirstPlugin {
                 bool newState = GUILayout.Toggle(state, "Enable");
                 GUILayout.EndHorizontal();
 
-
                 if (newState != state && kvp.Key != null) {
                     instanceStates[kvp.Key] = newState;
 
+                    // Restore original stats if toggled off
+                    if (!newState) {
+                        RestoreOriginalStats(kvp.Key);
+                    }
                 }
             }
 
@@ -62,15 +62,34 @@ namespace MyFirstPlugin {
 
         public void RegisterInstance(Players_FindNearestAndAttack instance) {
             if (!instanceStates.ContainsKey(instance)) {
-                instanceStates.Add(instance, true); 
+                instanceStates.Add(instance, true);
+
+                // Save original stats
+                originalStats[instance] = (instance.Hp, instance.critChance, instance.lifeSteal);
+
                 Logger.LogInfo($"Registered instance: {instance.name}");
+                Logger.LogInfo($"Original stats: {originalStats[instance]} ");
             }
         }
 
         public void UnregisterInstance(Players_FindNearestAndAttack instance) {
             if (instanceStates.ContainsKey(instance)) {
                 instanceStates.Remove(instance);
+
+                // Remove from original stats
+                originalStats.Remove(instance);
+
                 Logger.LogInfo($"Unregistered instance: {instance.name}");
+            }
+        }
+
+        public void RestoreOriginalStats(Players_FindNearestAndAttack instance) {
+            if (originalStats.TryGetValue(instance, out var stats)) {
+                // instance.Hp = stats.Hp;
+                instance.critChance = stats.critChance;
+                instance.lifeSteal = stats.lifeSteal;
+
+                Logger.LogInfo($"Restored original stats for instance: {instance.name}");
             }
         }
     }
@@ -90,29 +109,25 @@ namespace MyFirstPlugin {
         public static void PostfixOnDestroy(Players_FindNearestAndAttack __instance) {
             if (HeroPatchWithUI.Instance != null) {
                 HeroPatchWithUI.Instance.UnregisterInstance(__instance);
-                Logger.LogInfo($"Unregistered instance: {__instance.name}");
             }
         }
-
 
         [HarmonyPatch("Update"), HarmonyPrefix]
         public static void PrefixUpdate(Players_FindNearestAndAttack __instance) {
             if (HeroPatchWithUI.Instance != null &&
-                HeroPatchWithUI.Instance.enablePatch &&
-                HeroPatchWithUI.Instance.instanceStates.TryGetValue(__instance, out bool isEnabled) &&
-                isEnabled) {
-                __instance.maxHp = 9999;
-                __instance.Hp = 9999;
-                __instance.critChance = 1;
-                __instance.lifeSteal = 20;
-                __instance.base_critChance = 1;
-                __instance.base_lifeSteal = 20;
+                HeroPatchWithUI.Instance.instanceStates.TryGetValue(__instance, out bool isEnabled)) {
+
+                if (HeroPatchWithUI.Instance.enablePatch && isEnabled) {
+                    // Apply modified stats
+                    __instance.Hp = 9999;
+                    __instance.critChance = 1; // 100%
+                    __instance.lifeSteal = 2; // 200%
+                }
+                else {
+                    // Restore original stats
+                    HeroPatchWithUI.Instance.RestoreOriginalStats(__instance);
+                }
             }
         }
     }
 }
-
-
-
-
-
